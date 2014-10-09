ada53ccf50d9165e81c8d432fb1d902928cd743b
Paul Cowan <paul@custardsource.com>
2014-10-10 09:20:32 +1100

    More robust global state to avoid silent data failuter

--- ScratchKeeper/src/com/custardsource/scratchminder/GlobalState.java
+++ ScratchKeeper/src/com/custardsource/scratchminder/GlobalState.java
@@ -1,7 +1,12 @@
 package com.custardsource.scratchminder;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import com.google.common.io.ByteStreams;
 
 import android.app.Application;
 import android.content.Context;
@@ -10,7 +15,7 @@ public class GlobalState extends Application {
 	private static final String STATE_FILE = "savedstate";
 
 	private Lobby lobby;
-	
+
 	public GlobalState() {
 		super();
 	}
@@ -20,22 +25,41 @@ public class GlobalState extends Application {
 	}
 
 	private void restoreGameState() {
+		doBackup();
 		try {
 			ObjectInputStream inputStream = new ObjectInputStream(
 					openFileInput(STATE_FILE));
+
 			lobby = (Lobby) inputStream.readObject();
 			inputStream.close();
 			lobby.resetIfNecessary();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// OK; new install, no data.
 			lobby = new Lobby();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void doBackup() {
+		try {
+			InputStream inputStream = 
+					openFileInput(STATE_FILE);
+			OutputStream outputStream = 
+					openFileOutput(STATE_FILE + ".bak", Context.MODE_PRIVATE);
+			ByteStreams.copy(inputStream, outputStream);
+			inputStream.close();
+			outputStream.close();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 	}
 
 	void flush() {
 		saveGameState();
 	}
-	
+
 	private void saveGameState() {
 		try {
 			ObjectOutputStream outputStream = new ObjectOutputStream(
